90af88474f53a63d636cf91f0792d7d4ea19e852
anukzak <anahit_zakaryan@edu.aua.am>
2023-05-15 15:21:58 +0400

    Bass model

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+# Add install requirements
+setup(
+    author="Anahit Zakaryan",
+    description="A package for Bass model function.",
+    name="bassmodel",
+    packages=find_packages(include=["impyrial", "impyrial.*"]),
+    version="0.1.0",
+    install_requires=['numpy>=1.10', 'pandas','matplotlib.pyplot','math','scipy.optimize'],
+)
